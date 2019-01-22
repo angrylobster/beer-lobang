38ab4813a26ced705fd5258cc4646b5cf455f330
angrylobster <liewminshan@gmail.com>
2019-01-22 09:45:11 +0800

    Heroku deployment code

--- db.js
+++ db.js
@@ -4,13 +4,30 @@ const locations = require('./models/locations');
 const beers = require('./models/beers');
 const url = require('url');
 
-const configs = {
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'beerlobang_db',
-    port: 5432,
-    password: 'postgres'
-};
+if( process.env.DATABASE_URL ){
+    //we need to take apart the url so we can set the appropriate configs
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+  
+    //make the configs object
+    var configs = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true
+    };
+  }else{
+    //otherwise we are on the local network
+    var configs = {
+        user: 'postgres',
+        host: '127.0.0.1',
+        database: 'beerlobang_db',
+        port: 5432,
+        password: 'postgres'
+    };
+  }
 
 const pool = new pg.Pool(configs);
 
